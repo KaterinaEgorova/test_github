b0c588dd2a4af7812c9b1578fb730cbcfb029474
Ekaterina Egorova <kate.egorova@gmail.com>
2013-03-14 20:35:28 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGithab::Application.config.secret_token = '8e44462d51b7f840c9d008832add3267ad23555ba33ba56a6cce4ef8b89f22a99d52645ce4d46522252795a6422303f9c6c5a23daac19ab0463d8c5147d969a2'
